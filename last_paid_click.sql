f3ecb871a22148045edc69f308a8d01b53ba296d
Andrey Brylyakov <31739979+IcePenguinDiscoPanda@users.noreply.github.com>
2023-10-05 21:48:34 +0300

    fixed 1 task

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,35 +1,42 @@
-with prom as (
+with visitors_and_leads as (
     select
-        distinct on (s.visitor_id)
-        s.visitor_id,
         s.visit_date,
-        'source' as utm_source,
+        s.source as utm_source,
         s.medium as utm_medium,
         s.campaign as utm_campaign,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
-        l.status_id
+        l.status_id,
+        row_number()
+            over (partition by s.visitor_id order by s.visit_date desc)
+        as rn
     from sessions as s
     left join leads as l
-        on s.visitor_id = l.visitor_id
-        and l.created_at >= s.visit_date 
-where s.medium in (
-    'cpc',
-    'cpm',
-    'cpa',
-    'youtube',
-    'cpp',
-    'tg',
-    'social'
+        on
+            s.visitor_id = l.visitor_id
+            and s.visit_date <= l.created_at
+    where s.medium in (
+        'cpc',
+        'cpm',
+        'cpa',
+        'youtube',
+        'cpp',
+        'tg',
+        'social'
+    )
 )
-)
-select *
-from prom
-order by
-amount desc nulls last,
-visit_date asc,
-utm_source asc,
-utm_medium asc,
-utm_campaign asc
+select
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+from visitors_and_leads where rn = 1
+order by 7 desc nulls last, 1, 2, 3, 4
+limit 10
